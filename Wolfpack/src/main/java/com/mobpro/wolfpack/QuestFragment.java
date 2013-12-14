e91c766a367e5ed16564bebbcd1df2d947fca78d
Chloe Vilain <chloe.vilain@students.olin.edu>
2013-12-13 20:58:27 -0500

    Making the quest frag

--- Wolfpack/src/main/java/com/mobpro/wolfpack/QuestFragment.java
+++ Wolfpack/src/main/java/com/mobpro/wolfpack/QuestFragment.java
@@ -1,9 +1,43 @@
 package com.mobpro.wolfpack;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Chloe Local on 12/9/13.
  */
 public class QuestFragment extends Fragment {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.game_fragment, container, false);
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.imageView);
+        Button conquer = (Button)rootView.findViewById(R.id.button);
+
+        conquer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //go to game view
+            }
+        });
+
+        return rootView;
+
+    }
+
+    private int getStatus(){
+        return 0;
+    }
 }
