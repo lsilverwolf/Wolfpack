11db7c2347b8ac4595f53fa10ea7b005f46e1ec8
Chloe Vilain <chloe.vilain@students.olin.edu>
2013-12-16 19:47:07 -0500

    Game and Quest fragment launch

--- Wolfpack/src/main/java/com/mobpro/wolfpack/QuestFragment.java
+++ Wolfpack/src/main/java/com/mobpro/wolfpack/QuestFragment.java
@@ -1,6 +1,7 @@
 package com.mobpro.wolfpack;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,18 +23,32 @@ public class QuestFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        final View rootView = inflater.inflate(R.layout.game_fragment, container, false);
-        ImageView imageView = (ImageView) rootView.findViewById(R.id.imageView);
-        Button conquer = (Button)rootView.findViewById(R.id.button);
+        final View rootView = inflater.inflate(R.layout.quest_fragment, container, false);
+        //ImageView imageView = (ImageView) rootView.findViewById(R.id.imageView);
+        Button conquer = (Button)rootView.findViewById(R.id.conquer);
 
         conquer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //go to game view
+                // Create new fragment and transaction
+                Fragment newFragment = new GameFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+// Replace whatever is in the fragment_container view with this fragment,
+// and add the transaction to the back stack
+                transaction.replace(R.id.container, newFragment);
+                transaction.addToBackStack(null);
+
+// Commit the transaction
+                transaction.commit();
+
             }
         });
 
+
         return rootView;
+
 
     }
 
